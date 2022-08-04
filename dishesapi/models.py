d86f06dd51ac6d7a4cf5bbf951cb1e383cef29df
Vineesh <vineeshm199812@gmail.com>
2022-08-04 23:02:01 +0530

    modrestarntprjt

--- dishesapi/models.py
+++ dishesapi/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -10,3 +10,18 @@ class Dishes(models.Model):
 
     def __str__(self):
         return self.dishname
+
+class Reviews(models.Model):
+    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    dish=models.ForeignKey(Dishes,on_delete=models.CASCADE)
+    review=models.CharField(max_length=120)
+    rating=models.PositiveIntegerField()
+    date=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.review
+
+class Carts(models.Model):
+    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    dish=models.ForeignKey(Dishes,on_delete=models.CASCADE)
+    date=models.DateField(auto_now_add=True)
